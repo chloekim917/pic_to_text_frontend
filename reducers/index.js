882a90c418b062f989bff0dba16e43d558b2aa6d
chloekim917 <chloekim917@gmail.com>
2020-11-23 15:49:05 -0500

    organize

--- reducers/index.js
+++ reducers/index.js
@@ -1,5 +1,3 @@
-
-
 const initialState = {
     notebooks: [],
     currentNotebook: null,
@@ -11,7 +9,6 @@ const initialState = {
     confidence: '',
     translated: '',
 }
-
 
 const reducer  = (prevState=initialState, action) => {
     switch(action.type){
@@ -52,48 +49,3 @@ export default reducer;
 
 
 
-
-
-
-
-// // import { combineReducers } from 'redux';
-
-// const initialState = {
-//     notebooks: [],
-//     currentNotebook: 1,
-//     notes: [],
-//     currentNote: 1
-// }
-
-
-// const reducer  = (prevState=initialState, action) => {
-//     switch(action.type){
-//         case 'FETCH_NOTES':
-//             return {...prevState, notes: action.payload.notes}
-//         case 'SELECT_NOTE':
-//             return{...prevState, currentNote: action.payload.value}
-//         case 'FETCH_NOTEBOOKS':
-//             return {...prevState, notebooks: action.payload.notebooks}
-//         case 'SELECT_NOTEBOOK':
-//             return{...prevState, currentNotebook: action.payload.value}
-//         default:
-//             return prevState
-//     }
-// }
-
-// export default reducer;
-
-
-
-
-// import { combineReducers } from 'redux';
-// import notebookReducers from './notebookReducers'
-// import noteReducers from './noteReducers'
-
-
-// const allReducers = combineReducers({ 
-// notebooks: notebookReducers,
-// notes: noteReducers
-// })
-
-// export default allReducers;
